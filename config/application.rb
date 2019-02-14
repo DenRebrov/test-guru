d857c5ea49be6f0c08d2068963ff8de4a638bdc2
DenRebrov <den_rebrov@list.ru>
2019-02-14 15:37:20 +0300

    First task

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,9 @@ module TestGuru
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Don't generate system test files.
+    config.time_zone = 'Moscow'
+
+        # Don't generate system test files.
     config.generators.system_tests = nil
   end
 end
