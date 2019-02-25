6d2845bc6dfda1bbfc8dc7ca4ec0c0961b047a99
DenRebrov <den_rebrov@list.ru>
2019-02-25 16:44:48 +0300

    Task 8. Fix

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,6 +1,2 @@
 module TestPassagesHelper
-
-  def success?(test_passage)
-    test_passage.correct_questions * 100 / test_passage.test.correct_answers_count >= TestPassage::SUCCESS_RATE
-  end
 end
--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -5,8 +5,7 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
-  before_update :next_question
+  before_validation :before_validation_set_first_question, on: [:create, :update]
 
   def accept!(answer_ids)
     if correct_answer?(answer_ids)
@@ -20,6 +19,10 @@ class TestPassage < ApplicationRecord
     current_question.nil?
   end
 
+  def success?
+    correct_questions * 100 / test.correct_answers_count >= SUCCESS_RATE
+  end
+
   def total_questions
     test.questions.size
   end
@@ -30,10 +33,6 @@ class TestPassage < ApplicationRecord
 
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
-  end
-
   def correct_answer?(answer_ids)
     correct_answers.ids.sort == answer_ids.map(&:to_i).sort if answer_ids != nil
   end
@@ -42,7 +41,11 @@ class TestPassage < ApplicationRecord
     current_question.answers.correct
   end
 
+  def before_validation_set_first_question
+    self.current_question = next_question
+  end
+
   def next_question
-    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
+    test.questions.order(:id).where('id > ?', current_question.nil? ? 0 : current_question.id).first
   end
 end
