137fb99effbfff37ae5aead149572b729ea1059a
DenRebrov <den_rebrov@list.ru>
2019-03-06 16:43:04 +0300

    Task 16. Prepare to deploy on Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20190301150748) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: false
     t.string "body"
