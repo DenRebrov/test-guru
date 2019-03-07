0f3d0bfe0ae56eb6ecda5a052a336840c7ed2058
DenRebrov <den_rebrov@list.ru>
2019-03-07 10:23:37 +0300

    Add config action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,13 +63,12 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: "afternoon-savannah-57049.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
-
   config.action_mailer.smtp_settings = {
     adress:               "smtp.gmail.com",
     port:                 587,
-    authentication:       "plain",
     user_name:            ENV['SMTP_USERNAME'],
     password:             ENV['SMTP_PASSWORD'],
+    authentication:       "plain",
     enable_starttls_auto: true
 }
 
