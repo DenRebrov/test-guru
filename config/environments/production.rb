d3afd4ca21da035d0e8020a8d005aa6eeab76533
DenRebrov <den_rebrov@list.ru>
2019-03-08 01:31:30 +0300

    add advanced smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,20 +61,23 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "test-guru_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: "afternoon-savannah-57049.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: 'afternoon-savannah-57049.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-    adress:               "smtp.gmail.com",
+    adress:               'smtp.gmail.com',
     port:                 587,
+    domain:               "afternoon-savannah-57049.herokuapp.com",
     user_name:            ENV['SMTP_USERNAME'],
     password:             ENV['SMTP_PASSWORD'],
-    authentication:       "plain",
+    authentication:       'plain',
     enable_starttls_auto: true
 }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
