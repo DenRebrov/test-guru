dc8fee2c80033843c123138b019fabeadfa0f1f4
DenRebrov <den_rebrov@list.ru>
2019-03-07 09:56:39 +0300

    Add smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "test-guru_#{Rails.env}"
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: "afternoon-savannah-57049.herokuapp.com" }
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    adress:               "smtp.gmail.com",
+    port:                 587,
+    authentication:       "plain",
+    user_name:            ENV['SMTP_USERNAME'],
+    password:             ENV['SMTP_PASSWORD'],
+    enable_starttls_auto: true
+}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
