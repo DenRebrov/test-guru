a8bf9133c8fa858738797fdd0e66829086805e1c
DenRebrov <den_rebrov@list.ru>
2019-03-08 15:30:00 +0300

    Add feedback feature. Test

---
+++ app/mailers/feedbacks_mailer.rb
@@ -0,0 +1,16 @@
+class FeedbacksMailer < ApplicationMailer
+
+  def send_feedback(feedback)
+    @user = feedback.user
+    @body = feedback.body
+    @title = feedback.title
+
+    @admin = User.find_by(type: 'Admin')
+
+    if @admin.present?
+      mail to: @admin.email
+    else
+      redirect_to root_path, notice: 'Создайте администратора!'
+    end
+  end
+end
