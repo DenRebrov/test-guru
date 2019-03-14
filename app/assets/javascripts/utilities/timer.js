15b826db463ca6deeab89d77f3cd51a44dbe426e
DenRebrov <den_rebrov@list.ru>
2019-03-14 16:04:51 +0300

    Timer done

--- app/assets/javascripts/utilities/timer.js
+++ app/assets/javascripts/utilities/timer.js
@@ -1,10 +1,24 @@
-// document.addEventListener('turbolinks:load', function() {
-//     var control = document.getElementById('timer');
-//
-//     if (control) { startTimer(control) }
-// });
-//
-// function startTimer() {
-//     var startTime = control.dataset.start_time;
-//     alert(startTime);
-// }
+document.addEventListener('turbolinks:load', function() {
+    var control = document.getElementById('timer');
+
+    if (control) { startTimer(control) }
+});
+
+function startTimer(control) {
+    var startTime = control.dataset.startTime;
+    var endTime = control.dataset.endTime;
+    var spentTime = Math.trunc(Date.now() / 1000) - startTime;
+    var remainingTime = endTime - spentTime;
+
+    control.innerHTML--;
+
+    if (control.innerHTML == 0) {
+        window.location.replace(window.location.href + '/result');
+        setTimeout(function(){}, 1000)
+    } else {
+        setTimeout(startTimer, 1000, control)
+    }
+
+    control.innerHTML = remainingTime;
+    setTimeout(startTimer, 1000, control)
+}
